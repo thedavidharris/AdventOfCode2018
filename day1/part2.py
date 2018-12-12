c5400e932483045ca9837899a04fec1fe7d63b0d
David Harris <daharr@umich.edu>
2018-12-11 20:36:57 -0500

    Fix a regression

--- day1/part2.py
+++ day1/part2.py
@@ -4,4 +4,7 @@ seen = {0}
 freq = 0
 for line in cycle(open('input.txt').read().splitlines()):
     freq += int(line)
-    print(freq) if freq in seen else seen.add(freq)
+    if freq in seen:
+        print(freq)
+        break
+    seen.add(freq)
